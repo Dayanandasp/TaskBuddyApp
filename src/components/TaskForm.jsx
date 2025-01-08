69bd71f620cfaf9e1cee580a6d4bbc69dfe16503
Dayanandasp <dayanandasp426@gmail.com>
2025-01-08 12:14:52 +0530

    Added new ReactJS Project

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react"; 
+
+function TaskForm({ addTask }) { 
+    const [task, setTask] = useState(''); 
+    const [priority, setPriority] = useState("Medium"); 
+    const [category, setCategory] = useState("General"); 
+    const handleSubmit = (e) => { 
+        e.preventDefault(); 
+            addTask({ text: task, priority, category, 
+                completed: false });
+                 setTask(""); 
+                 setPriority("Medium"); 
+                 setCategory("General"); 
+                } 
+            
+            return ( 
+            <form onSubmit={handleSubmit} className="task-form"> 
+            <div> 
+                <input type="text" 
+                value={task} 
+                onChange={(e) => setTask(e.target.value)} 
+                placeholder="Enter your task" 
+                required 
+                />
+                <span><button type="submit">Add Task</button> 
+                </span>
+                </div> 
+                
+                <div id="btns"> 
+                    <select onChange={(e) => 
+                    setPriority (e.target.value)}> 
+                    <option value="High">High</option> 
+                    <option value="Medium">Medium</option> 
+                    <option value="Low">Low</option> 
+                    </select> 
+                    <select onChange={(e) => 
+                    setCategory(e.target.value)}> 
+                    <option value="General">General</option> 
+                    <option value="Work">Work</option> 
+                    <option value="Personal">Personal</option> 
+                    </select> 
+                </div> 
+            </form> 
+        ); 
+    } 
+                
+export default TaskForm;
